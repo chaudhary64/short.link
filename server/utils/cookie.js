4fb352620b4b40043ff8b615e029eeac3a85948d
chirag chaudhary <chiragchoudhary64@gmail.com>
2026-07-05 12:33:25 +0530

    Fix cross-site cookie policy

--- server/utils/cookie.js
+++ server/utils/cookie.js
@@ -1,6 +1,6 @@
 export const cookieOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
 }
